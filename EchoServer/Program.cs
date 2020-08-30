34f0fe45c1387d7e8f6754f71ea04989646a70d9
Michael Faber Harder <mich719p@edu.easj.dk>
2020-08-30 09:21:40 +0200

    Add project files.

---
+++ EchoServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EchoServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Server server = new Server();
+			server.Start();
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ EchoServer/Server.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoServer
+{
+	class Server
+	{
+
+		public void Start()
+		{
+			TcpListener server = new TcpListener(IPAddress.Loopback, 7);
+			server.Start();
+			using TcpClient socket = server.AcceptTcpClient();
+			{
+				Stream ns = socket.GetStream();
+				StreamReader sr = new StreamReader(ns);
+				StreamWriter sw = new StreamWriter(ns);
+				String line = sr.ReadLine();
+				sw.WriteLine(line);
+				sw.Flush();
+			}
+		}
+	}
+}
